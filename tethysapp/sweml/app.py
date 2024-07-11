0f33abcff3b1dfce73e82ce7484d3afebe7f3643
karnesh <jain.karnesh@gmail.com>
2024-07-11 12:53:16 -0400

    Started SWE tethys app

---
+++ tethysapp/sweml/app.py
@@ -0,0 +1,18 @@
+from tethys_sdk.base import TethysAppBase
+
+
+class Sweml(TethysAppBase):
+    """
+    Tethys app class for SWEML.
+    """
+
+    name = 'SWEML'
+    description = 'App to display 1-km resolution SWE prediction from deep learning models'
+    package = 'sweml'  # WARNING: Do not change this value
+    index = 'home'
+    icon = f'{package}/images/Superior.jpg'
+    root_url = 'sweml'
+    color = '#c23616'
+    tags = '"SWE", "Deep learning", "1-km resolution"'
+    enable_feedback = False
+    feedback_emails = []
